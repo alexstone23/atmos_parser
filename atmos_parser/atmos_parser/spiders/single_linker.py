349eb9182d1385829bcd777bee8600eb21136284
alexstone23 <melnychuckalex@gmail.com>
2018-02-02 20:40:58 +0200

    single page parser

---
+++ atmos_parser/atmos_parser/spiders/single_linker.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider
+from ..items import AtmosParserSingleItem
+import re
+
+
+class SingleLinkerSpider(CrawlSpider):
+    name = 'single_linker'
+    allowed_domains = ['atmos.illinois.edu']
+    start_urls = ['https://www.atmos.illinois.edu/cms/One.aspx?portalId=127458&pageId=151114']
+    custom_settings = {
+        'FEED_EXPORT_FIELDS': ['full_name', 'phone', 'email', 'biography']
+    }
+
+    def parse(self, response):
+        for r in response.xpath('//div[@id="page_content"]'):
+            items = AtmosParserSingleItem()
+            full_name = r.xpath('//div[@id="full_name"]/h1/text()').extract()
+            biography = r.xpath('//div[@id="news_content_body"]').xpath('//div[@class="img-responsive"]//text()').extract()
+            contact_div = r.xpath('//div[@id="contact_info"]')
+            items['email'] = contact_div.xpath('//a[@id="email"]/text()').extract()
+            items['full_name'] = full_name
+            items['biography'] = biography
+            phone = r.xpath('//div[@id="contact_info"]//text()').extract()
+            phone = ''.join(phone)
+            items['phone'] = re.findall('([\S][\d][\S]??\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', phone)
+            yield items
